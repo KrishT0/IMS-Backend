ff2e79df15a26d1e88c201142db19c4d10275bb5
Krishna-windows <krshnabiswal619@gmail.com>
2024-06-27 00:09:55 +0530

    fix : fixed CORS error

--- src/index.ts
+++ src/index.ts
@@ -2,6 +2,7 @@ import express, { Express } from "express";
 import dotenv from "dotenv";
 import connectDB from "./db";
 import morgan from "morgan";
+import cors from "cors";
 
 import internRouter from "./routes/intern.routes";
 import authRouter from "./routes/auth.routes";
@@ -14,6 +15,7 @@ const port = process.env.PORT || 3000;
 
 connectDB().then(() => {
   app.use(morgan("dev"));
+  app.use(cors());
   app.use(express.json());
   app.use(express.urlencoded({ extended: true }));
 
